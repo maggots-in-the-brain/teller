2f11950ebe2f7517e14e236d2c1b47bb2038f54f
TaroYamada <183961351+maggots-in-the-brain@users.noreply.github.com>
2025-03-24 07:32:47 +0000

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'public/users/sessions',
+    registrations: 'public/users/registrations'
+  }
+
   scope module: :public do
     root 'homes#top'
   end
