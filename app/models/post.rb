c69f50a85270c54eb3d59b57ba564b147f567bb5
TaroYamada <183961351+maggots-in-the-brain@users.noreply.github.com>
2025-03-31 05:17:42 +0000

    map

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode
+
 end
